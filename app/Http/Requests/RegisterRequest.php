4d7251d8394beebede6da96655fbe3dcf797d00d
kei1476 <kei1476to@outlook.jp>
2023-04-11 13:23:31 +0900

    記述ミス修正

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -24,9 +24,9 @@ class RegisterRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => ['required|string|max:255'],
-            'email' => ['required|string|email|max:255|unique:users'],
-            'password' => ['required|max:255|min:8|regex:/^(?=.*?[a-z])(?=.*?\d)[a-z\d]+$/i'],
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|max:255|min:8|regex:/^(?=.*?[a-z])(?=.*?\d)[a-z\d]+$/i',
         ];
     }
 }
